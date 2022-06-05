1c475103ec407c05b3e08ed5b606c6f0329a4821
Mpaogbuji bethel <bethelogbuji84@gmail.com>
2022-06-05 23:41:44 +0100

    hid my secret key

--- Bethel_Mpa/Bethel_Mpa/settings.py
+++ Bethel_Mpa/Bethel_Mpa/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uyw7#w+n@a0tns)u9$y$l-+x3*_^3=$0g#lp)@2vpkkf1%gbb('
+SECRET_KEY = os.getenv.__get__("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv.__get__("DEBUG")
 
 ALLOWED_HOSTS = []
 
